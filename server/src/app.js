b977afece452cd6813752bf8e5446c4f4db3ec2b
ranasamiafzal <ranasami0909@gmail.com>
2026-06-24 11:35:30 +0500

    feat: update CORS configuration to support dynamic origins and Vercel deployments

--- server/src/app.js
+++ server/src/app.js
@@ -161,19 +161,27 @@ app.use('/api/v1/payment', paymentLimiter);
 app.use('/api/v1/aiMatch', aiLimiter);
 app.use('/api/v1/admin', adminLimiter);
 
-// we use cors  for setting that which origin we will accept the req 
+// we use cors for setting that which origin we will accept the req 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:3002",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001",
-        "http://127.0.0.1:3002",
-        process.env.CORS_ORIGIN
-    ].filter(Boolean),
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "http://localhost:3002",
+            "http://127.0.0.1:5173",
+            "http://127.0.0.1:3000",
+            "http://127.0.0.1:3001",
+            "http://127.0.0.1:3002",
+            process.env.CORS_ORIGIN
+        ].filter(Boolean);
+
+        if (!origin || allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }))
 
